5d886fb069538cbe1f371480f498abfc31e1fb65
PopisojMM <66846834+PopisojMM@users.noreply.github.com>
2020-06-12 18:51:54 -0300

    Update Ejerc2FACIL.py

--- Ejerc2FACIL.py
+++ Ejerc2FACIL.py
@@ -1,11 +1,11 @@
-i=0
-while i<5:
-    i=i +1
+contraseña=0
+while contraseña<5:
+    contraseña=contraseña +1
     clave=input("ingrese su clave")
     if str(clave)=="Info2020":
         print("CLAVE CORRECTA")
         break
     else:
         print("CLAVE INCORRECTA")
-        if i==5:
+        if contraseña==5:
             print("CUENTA BLOQUEADA")
